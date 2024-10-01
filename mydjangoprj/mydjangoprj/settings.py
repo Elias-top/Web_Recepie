96a1134345ff0f40518b749a6174276a011a0ace
Elias-top <bkmz1999ilyxa@gmail.com>
2024-10-02 00:22:46 +0300

    Финальная версия с nginx и gunicorn собрано через compose

--- mydjangoprj/mydjangoprj/settings.py
+++ mydjangoprj/mydjangoprj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b23ur7@v!*h)v66y=a5=-na3=^hgy*pm+8+#d$5xvk6qtznmq4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','193.108.115.147']
 
